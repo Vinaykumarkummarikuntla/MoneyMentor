c45c0d67c5c3588b9eb1e09b19d2df4248fc32e8
Vinaykumarkummarikuntla <kvk.9618@gmail.com>
2023-05-31 17:56:58 +0530

    hashing implemented

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const bcrypt = require("bcrypt");
+
 const sequelize = require("./util/database");
 const signup = require("./models/signupmodel");
 
@@ -13,21 +15,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+// storing user details in database
 app.post("/signupdetails", async (req, res, next) => {
   try {
     const username = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
 
+    console.log("the storing details are " + username + " and " + email);
+
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    console.log("hashedpassword", hashedPassword);
+
     const data = await signup.create({
       username: username,
       email: email,
-      password: password,
+      password: hashedPassword,
     });
     res.status(200).json({ signupdetails: data });
   } catch (err) {
     console.error(err);
   }
+
+  //   res.sendFile('./public/login/index.html');
 });
 
 app.post("/logindetails", async (req, res, next) => {
@@ -35,26 +47,30 @@ app.post("/logindetails", async (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    const user= await signup
-      .findOne({
-        where: {
-          email: email,
-        },
-      })
-      .then((user) => {
-        if (user) {
-          if (user.password === password) {
-            res.status(200).json({ logindetails: user });
-            console.log("Password is correct");
-          } else {
-            console("Password is incorrect");
-            res.status(401).json({ error: "User not authorized" });
-          }
-        } else {
-          console.log("User is does not exist");
-          res.status(404).json({ error: "User is not found" });
-        }
-      });
+    const user = await signup.findOne({
+      where: {
+        email: email,
+      },
+    });
+    if (user) {
+      // console.log("Stored hashed password:", user.password);
+      // console.log("Entered password:", password);
+
+      const isMatched = await bcrypt.compare(password, user.password);
+
+      // console.log("Password comparison result:", isMatched);
+
+      if (isMatched) {
+        res.status(200).json({ logindetails: user });
+        console.log("Password is correct");
+      } else {
+        console.log("Password is incorrect");
+        res.status(401).json({ error: "User not authorized" });
+      }
+    } else {
+      console.log("User is does not exist");
+      res.status(404).json({ error: "User is not found" });
+    }
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Internal server error" });
@@ -62,7 +78,7 @@ app.post("/logindetails", async (req, res, next) => {
 });
 
 sequelize
-  .sync()
+  .sync({})
   .then((response) => {
     console.log(response);
     app.listen(4000);
