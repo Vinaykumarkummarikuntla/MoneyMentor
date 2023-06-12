6ddae27069f35e256540a488639400ab121687df
Vinaykumarkummarikuntla <kvk.9618@gmail.com>
2023-06-12 17:25:46 +0530

    forgot password frontend

--- controllers/expense.js
+++ controllers/expense.js
@@ -8,7 +8,7 @@ const sequelize = require('../util/database')
 
 // storing expense details
 exports.expense = async (req, res, next) => {
- let  t = sequelize.transaction()
+  const t = await sequelize.transaction()
   try {
     const expenseAmount = req.body.expenseAmount
     const checkDescription = req.body.checkDescription
@@ -19,7 +19,9 @@ exports.expense = async (req, res, next) => {
       category,
       description: checkDescription,
       signupId: req.user.id
-    })
+    }
+    // { transaction: t }
+    )
     // try {
     const user = await User.findByPk(req.user.id)
     if (user) {
@@ -27,26 +29,30 @@ exports.expense = async (req, res, next) => {
       // console.log("previoustotal amount ",previosTotalAmount)
       const currentTotalAmount = Number(previousTotalAmount) + Number(expenseAmount)
       // console.log("currentTotalAmount" ,currentTotalAmount)
-      await User.update({ totalAmount: currentTotalAmount }, { where: { id: req.user.id } })
+      await User.update({ totalAmount: currentTotalAmount }, { where: { id: req.user.id } }
+        // { transaction: t }
+        )
       // res.status(200).json({ expense })
     }
     // } catch (error) {
     //   logger.error('An error occurred:', error)
     //   res.status(500).json({ success: false, error })
     // }
-    // 
-    await t.then(function(transaction) {
-       transaction.commit();
-      console.log('the expense details are stored');
-      res.status(200).json({ msg: 'success' });
-    });
+    //
+
+    // await t.commit()
+    console.log('the expense details are stored')
+    res.status(200).json({ msg: 'success' })
   } catch (err) {
-    
-    await t.rollback()
+    // if (t) {
+    //   await t.rollback()
+    // }
     console.error(err)
     res.status(500).json({ msg: err })
   }
 }
+
+
 
 // get expense details
 exports.getexpense = async (req, res, next) => {
@@ -64,18 +70,34 @@ exports.getexpense = async (req, res, next) => {
 
 // delete expense details
 exports.deleteexpense = async (req, res, next) => {
-  
+  const t = await sequelize.transaction()
   try {
     const expenseId = req.body.expenseId
     const expense = await Expense.findOne({ where: { signupId: req.user.id } })
     // const expense = await Expense.findAll({ where: { signupId: req.user.id}});
+    console.log('delete expense ------>', expense.expenseamount)
     if (expense) {
-      await expense.destroy()
+      await expense.destroy({ transaction: t })
       res.status(200).json({ msg: 'Expense deleted successfully' })
+
+      const user = await User.findByPk(req.user.id)
+      if (user) {
+        const previousTotalAmount = user.totalAmount
+        // console.log("previoustotal amount ",previosTotalAmount)
+        const currentTotalAmount = Number(previousTotalAmount) - Number(expense.expenseamount)
+        // console.log("currentTotalAmount" ,currentTotalAmount)
+        await User.update({ totalAmount: currentTotalAmount }, { where: { id: req.user.id } }, { transaction: t })
+        // res.status(200).json({ expense })
+      }
+
+      await t.commit()
     } else {
       res.status(404).json({ msg: 'Expense not found' })
     }
   } catch (err) {
+    if (t) {
+      await t.rollback()
+    }
     console.log(err)
     res.status(500).json({ error: 'An error occurred while deleting the expense' })
   }
--- public/login.js
+++ public/login.js
@@ -16,3 +16,51 @@ function checkOnServer (event) {
       console.log(err)
     })
 }
+
+
+//  forgot password
+
+document.addEventListener('DOMContentLoaded', function() {
+  var forgotPasswordLink = document.getElementById('forgotPasswordLink');
+
+  forgotPasswordLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    showForgotPasswordForm();
+  });
+
+  function showForgotPasswordForm() {
+    var form = document.createElement('form');
+
+    var emailInput = document.createElement('input');
+    emailInput.type = 'email';
+    emailInput.name = 'email';
+    emailInput.placeholder = 'Enter your email';
+    form.appendChild(emailInput);
+
+    var submitButton = document.createElement('button');
+    submitButton.type = 'submit';
+    submitButton.textContent = 'Reset Password';
+    form.appendChild(submitButton);
+
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+      var email = emailInput.value;
+      handleForgotPasswordFormSubmission(email);
+    });
+
+    document.body.appendChild(form);
+  }
+
+  function handleForgotPasswordFormSubmission(email) {
+    // Make an AJAX request to the backend API using Axios
+    axios.post('http://localhost:4000/password/forgotpassword', { email: email })
+      .then((response) => {
+        console.log(response.data);
+        // Handle the response from the server
+      })
+      .catch((error) => {
+        console.error(error);
+        // Handle any errors that occur during the request
+      });
+  }
+});
