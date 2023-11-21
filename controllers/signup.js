55ae1e5df464febb6285394dec29fcac2b1a3a60
Vinaykumarkummarikuntla <kvk.9618@gmail.com>
2023-11-21 20:37:45 +0530

    encryption and decryption added

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,12 +1,44 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 const signup = require('../models/signupmodel')
 const logger = require('../middleware/logger')
+
+
+// secret key for encryption and decryption
+encryptionKey = crypto.randomBytes(32).toString('hex')
+
+function generateRandomIV() {
+  return crypto.randomBytes(16);
+}
+
+// encrypt 
+function encryptData(data) {
+  const iv = generateRandomIV();
+  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(encryptionKey), iv);
+  let encryptedData = cipher.update(data, 'utf-8', 'hex');
+  encryptedData += cipher.final('hex');
+  return { encryptedData, iv: iv.toString('hex') };
+}
+
+// decrypt 
+function decryptData(encryptedData, iv) {
+  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(encryptionKey), Buffer.from(iv, 'hex'));
+  let decryptedData = decipher.update(encryptedData, 'hex', 'utf-8');
+  decryptedData += decipher.final('utf-8');
+  return decryptedData;
+}
+
 
 // TODO Signup details
 exports.signupdetails = async (req, res, next) => {
   try {
-    const username = req.body.username
-    const email = req.body.email
+    // old code
+    // const username = req.body.username
+    // const email = req.body.email
+
+    // new code   
+    const username = encryptData(req.body.username)
+    const email = encryptData(req.body.email)
     const password = req.body.password
     // console.log('the storing details are ' + username + ' and ' + email)
 
